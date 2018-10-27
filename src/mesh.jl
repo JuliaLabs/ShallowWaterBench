fa176e0146cbd5c7cbd65631304bd0aa7ac7c55c
Valentin Churavy <v.churavy@gmail.com>
2018-10-27 18:01:15 -0400

    add PartitionedMesh

--- src/mesh.jl
+++ src/mesh.jl
@@ -26,8 +26,63 @@ Base.mod(x::T, y::AbstractUnitRange{T}) where {T<:Integer} = y[mod1(x - y[1] + 1
 Base.mod(x::CartesianIndex{N}, y::CartesianIndices{N}) where {N} = CartesianIndex(ntuple(n->mod(x[n], axes(y)[n]), N))
 
 neighbor(cell, face, mesh::PeriodicCartesianMesh) =
-  (cell + face) in mesh.inds ? cell + face : mod(cell + face, mesh.inds)
+    (cell + face) in mesh.inds ? cell + face : mod(cell + face, mesh.inds)
 
 opposite(face, mesh::PeriodicCartesianMesh) = face * -1
 
 neighbors(cell, mesh::Mesh) = map(face -> neighbor(cell, face, mesh), faces(cell, mesh))
+
+"""
+    PartionedCartesianMesh
+
+Represents the local region of a CartesianMesh. 
+"""
+struct PartionedCartesianMesh{N} <: CartesianMesh{N}
+    inds :: CartesianIndices{N}
+end
+faces(cell, mesh::PartionedCartesianMesh{N}) where {N} = CartesianNeighbors{N}()
+opposite(face, mesh::PartionedCartesianMesh) = face * -1
+neighbor(cell, face, mesh::PartionedCartesianMesh) = cell + face
+
+parentindicies(mesh::PartionedCartesianMesh) = mesh.inds
+
+##
+# TODO:
+# 1. We probably need to translate from global indicies (parentindicies) to local indicies
+#    of a storage struct.
+# 2. Associate a ghost boundary with a particular process
+##
+
+"""
+    ghostboundary(mesh::PartionedCartesianMesh)
+
+Gives the non-local indicies that need to be updated
+"""
+function ghostboundary(mesh::PartionedCartesianMesh{N}) where N
+    fI = first(parentindicies(mesh))
+    lI = last(parentindicies(mesh))
+
+    upper = ntuple(N) do i
+        head, tail = select(fI, lI, i)
+        (head..., fI[i] - 1, tail...)
+    end
+
+    lower = ntuple(N) do i
+        head, tail = select(fI, lI, i)
+        (head..., lI[i] + 1, tail...)
+    end
+
+    return (upper..., lower...)
+end
+
+function select(fI, lI, i)
+    head = ntuple(i-1) do j
+        fI[j]:lI[j]
+    end
+
+    tail = ntuple(length(fI) - i) do j
+        fI[j]:lI[j]
+    end
+
+    return head, tail
+end
