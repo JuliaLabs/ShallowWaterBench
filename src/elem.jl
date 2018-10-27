1270064880cef3fd4742df5d7776450c5f241af7
Shashi Gowda <shashigowda91@gmail.com>
2018-10-27 15:12:11 -0400

    elem.jl

---
+++ src/elem.jl
@@ -0,0 +1,34 @@
+abstract type Element end
+
+struct RectElem{S<:AbstractArray} <: Element
+    coeffs::S
+end
+
+function Base.show(io::IO, e::RectElem)
+    print(io, "RectElem containing ")
+    show(io, MIME"text/plain"(), e.coeffs)
+end
+
+Base.@pure function face2idx(arr, f)
+    @assert ndims(arr) == length(f)
+    ntuple(ndims(arr)) do i
+        dir = f[i]
+        if dir == 0
+            Colon()
+        elseif dir == -1
+            1
+        elseif dir == 1
+            size(arr, i)
+        else
+            error("Only 1, 0 and -1 are allowed in face index")
+        end
+    end
+end
+
+function face(m::Mesh, e::RectElem, f)
+    e.coeffs[face2idx(e.coeffs, f)...]
+end
+
+Base.@inline function faceview(e::RectElem, f, m::Mesh)
+    view(e.coeffs, face2idx(e.coeffs, f)...)
+end
