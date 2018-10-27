9782c4d77eaafd9dd06143a3dda45a57dd15302c
Valentin Churavy <v.churavy@gmail.com>
2018-10-27 19:32:49 -0400

    add partitioning code

--- src/mesh.jl
+++ src/mesh.jl
@@ -50,7 +50,6 @@ parentindicies(mesh::PartionedCartesianMesh) = mesh.inds
 # TODO:
 # 1. We probably need to translate from global indicies (parentindicies) to local indicies
 #    of a storage struct.
-# 2. Associate a ghost boundary with a particular process
 ##
 
 """
@@ -85,4 +84,65 @@ function select(fI, lI, i)
     end
 
     return head, tail
+end
+
+"""
+    Partition
+
+Describes a partition over the data.
+
+```julia
+mpirank = MPI.Comm_rank(mpicomm)
+mpisize = MPI.Comm_size(mpicomm)
+
+dim = 2
+@assert mpisize % dim == 0
+sz = mpisize ÷ dim
+ranks = reshape(0:(mpisize-1), ntuple(i->sz, dim)...)
+
+P = Partition(CartesianIndices((1:1024, 1:1024)), ranks)
+inds = rankindices(mpirank)
+
+mesh = PartionedCartesianMesh(inds)
+for cells in ghostboundary(mesh)
+    other = locate(P, cells)
+    @show other, cells
+end
+```
+"""
+struct Partition{N}
+    indices    :: CartesianIndices{N}
+    ranks      :: Array{Int,N}                     # ranks[i]==r ⇒ rank r has piece i
+    partitions :: Array{CartesianIndices{N}, {N}}  # the indicies of piece i
+    function Partition{N}(inds::CartesianIndices{N}, ranks :: Array{Int,N})
+        partitions = partitions(inds, size(ranks))
+        new(inds, ranks, partitions)
+    end
+end
+
+function locate(p::Partition, I...)
+    i = findfirst(P->I in P, p.partitions)
+    p.ranks[i]
+end
+
+function rankindices(p::Partition, r)
+    i = findfirst(R-> r==R, p.ranks)
+    p.partitions[i]
+end
+
+function partitions(inds::CartesianIndices{N}, n::NTuple{N,Int}) where N
+    fI = first(inds)
+    lI = last(inds)
+
+    steps = ceil.(Int, size(inds) ./ n)
+    starts = ntuple(N) do i
+        fI[i]:steps[i]:lI[i]
+    end
+    map(CartesianIndices(starts)) do cI 
+        I = ntuple(N) do i
+            l = min(lI[i], cI[i]+steps[i])
+            cI[i]:l
+        end
+        CartesianIndices(I)
+    end
 end
