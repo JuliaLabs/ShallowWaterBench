380e741f532ecfb174e928835e3859a20efbad1b
Peter Ahrens <ptrahrens@gmail.com>
2018-10-31 13:16:35 -0400

    yeah maybe this will work

--- src/TotallyNotApproxFun.jl
+++ src/TotallyNotApproxFun.jl
@@ -1,35 +1,39 @@
-using FastGaussQuadrature.jl
+#using FastGaussQuadrature.jl
+using StaticArrays
 
 #A representation of a T-valued function in an N-Dimensional space.
 abstract type Fun{T, N} end
 
-(f::Fun)(x) = apply(f, SVector(x))
-(f::Fun)(x::Vararg{N}) = apply(f, SVector(x))
-
 #A discretization of a (T-valued function)-valued function in an N-Dimensional space.
-abstract type Basis{T, N} <: AbstractArray{<:Fun{T, N}} end
+abstract type Basis{T, N, F<:Fun{T, N}} <: AbstractArray{F, N} end
 
 #A function represented by a linear combination of basis functions
-struct ComboFun{T, N, B<:Basis{T, N}, C<:AbstractArray{T}}
+struct ComboFun{T, N, B<:Basis{<:Any, N}, C<:AbstractArray{T}} <: Fun{T, N}
     basis::B
     coeffs::C
 end
 
+(f::ComboFun)(x) = apply(f, SVector(x))
+(f::ComboFun)(x...)  = apply(f, SVector(x...))
 #f(x) is just sum_i(c_i * b_i(x))
-apply(f::ComboFun{T, N}, x::SVector{N})::T where {T, N} = sum(f.coeffs .* apply.(b, (x,)))
+function apply(f::ComboFun, x::AbstractVector)
+    println(f.coeffs)
+    println(f.basis)
+    println(Ref(x))
+    return sum(f.coeffs .* (apply.(f.basis, Ref(x))))
+end
 
 #A basis corresponding to a set of points, where the basis function i is one(T) at point i and zero(T) everywhere else
-abstract type OrthoBasis{T, N} <: Basis{T, N} end
+abstract type OrthoBasis{T, N, F} <: Basis{T, N, F} end
 
-for op in (+, -)
+for op in (:+, :-)
     @eval begin
         function Base.$(op)(a::ComboFun{T, N, B}) where {T, N, B <: OrthoBasis}
             ComboFun(a.basis, map($op, a.coeffs))
         end
     end
 end
-
-for op in (+, -, *, /)
+for op in (:+, :-, :*, :/)
     @eval begin
         function Base.$(op)(a::ComboFun{T, N, B}, b::ComboFun{S, N, B}) where {T, S, N, B <: OrthoBasis}
             @assert a.basis == b.basis
@@ -38,42 +42,96 @@ for op in (+, -, *, /)
     end
 end
 
-#A function which is a cartesian product of one-dimensional functions
-struct ProductFun{T, N, F <: NTuple{N, Fun{T, 1}}} <: Fun{T, N}
+#A function which is a product of one-dimensional functions
+struct ProductFun{T, N, F <: NTuple{N, <:Fun{<:Any, 1}}, Op} <: Fun{T, N}
+    op::Op
     funs::F
 end
 
-apply(f::ProductFun{T, N}, x::SVector{N})::T where {T, N} = prod(map(apply, f.funs, x))
+(f::ProductFun)(x) = apply(f, SVector(x))
+(f::ProductFun)(x...) = apply(f, SVector(x...))
+apply(f::ProductFun{T, N}, x::AbstractVector) where {T, N} = f.op(apply.(f.funs, x))
 
-
-
-#A function which is a cartesian product of one-dimensional Bases
-struct ProductBasis{T, N, B <: NTuple{N, OrthoBasis{T, 1}}} <: OrthoBasis{T, N}
+#A basis which is a cartesian product of one-dimensional bases
+struct ProductBasis{T, N, F, B <: NTuple{N, <:OrthoBasis{T, 1, <:F}}} <: OrthoBasis{T, N, F}
     bases::B
 end
 
 Base.size(b::ProductBasis) = map(length, b.bases)
-
 Base.getindex(b::ProductBasis, i::CartesianIndex) = ProductFun(map(getindex, b.basis, Tuple(i))...)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-struct LagrangeBasis{T, P} <: OrthoBasis{T, 1}
+#The minimum-degree polynomial function which is 1 at the Rth point and 0 at the other points
+struct LagrangeFun{T, P <: AbstractVector{T}, I} <: Fun{T, 1}
     points::P
 end
+
+LagrangeFun(points::AbstractVector, i) = LagrangeFun{eltype(points), typeof(points), i}(points)
+(f::LagrangeFun)(x) = apply(f, SVector(x))
+(f::LagrangeFun)(x...) = apply(f, SVector(x...))
+#This method is mostly here for clarity, it probably shouldn't be called (specialize this somewhere with a stable interpolation routine)
+apply(f::LagrangeFun, x::AbstractVector) = apply(f, x[1])
+function apply(f::LagrangeFun{T, P, I}, x) where {T, P, I}
+    @assert length(x) == 1
+    res = prod(ntuple(n->n == I ? 1 : (x - f.points[n])/(f.points[I] - f.points[n]), length(f.points)))
+end
+
+#A basis of polynomials
+struct LagrangeBasis{T, P <: AbstractVector{T}} <: OrthoBasis{T, 1, LagrangeFun{T, P}}
+    points::P
+end
+
+Base.size(b::LagrangeBasis) = map(length, b.points)
+Base.getindex(b::LagrangeBasis, i::Int) = LagrangeFun(b.points, i)
+
+#return a linear function which maps x₀ to y₀ and x₁ to y₁
+function repositioner(x₀::AbstractVector{T}, x₁::AbstractVector{T}, y₀::AbstractVector{T}, y₁::AbstractVector{T}) where {N, T}
+    ProductFun(Tuple(ComboFun.(LagrangeBasis.(SVector.(x₀, x₁)), SVector.(y₀, y₁))))
+end
+
+f = LagrangeFun(SVector(2.0, 4.0), 1)
+println(f(1))
+g = ComboFun(LagrangeBasis(SVector(2.0, 4.0)), SVector(10, 12))
+println(g(1))
+
+using InteractiveUtils
+@code_warntype(f(1))
+
+r = repositioner(SVector(2.0, 4.0), SVector(3.0, 5.0), SVector(-1.0, -1.0), SVector(1.0, 1.0))
+
+println(r(2.5, 4.1))
+
+
+
+
+
+
+
+
+
+
+#=
+for N = 1:16
+    points = gensym()
+    @eval begin
+        (foo, _) = lglpoints(i)
+        const points = SVector(foo)
+        
+        
+        struct LobattoPoints{T, N} <: AbstractVector{T, N} end
+        getindex(LobattoPoints{T, N}, i) = $points[i]
+    end
+end
+
+
+struct LagrangeFun{T, P <: AbstractVector} <: Fun{T, N}
+    points::P
+end
+
+struct LagrangeFun{T, P <: AbstractVector} <: Fun{T, N}
+    points::P
+end
+
+struct LobattoPoints{T, P}
 
 apply(f::ComboFun{T, 1, PolyBasis{S, P}}, x::SVector{1})::T where {T, S, P} = apply(f, x[1])
 
@@ -200,3 +258,4 @@ function volumerhs!(h′, V⃗′, h, V⃗, bathymetry, metric, D, ω, elems, gr
         end #i
     end #e ∈ elems
 end #function volumerhs-2d
+=#
