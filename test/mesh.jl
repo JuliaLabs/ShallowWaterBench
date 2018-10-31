f7f8563dbe4401712fbbe86160eb87517a33c4cf
Valentin Churavy <v.churavy@gmail.com>
2018-10-31 11:39:32 -0400

    add simple test for overelems

--- test/mesh.jl
+++ test/mesh.jl
@@ -8,4 +8,10 @@ const Dim = 2
 globalInds = CartesianIndices(ntuple(i-> 1:1024, Dim))
 globalMesh = PeriodicCartesianMesh(CartesianIndices(globalInds))
 
+storage = Array{Int64}(undef, map(length, axes(globalInds))...)
+
+overelems(globalMesh, storage) do elem, mesh, out
+    storage[elem] = 1
+end
+
 
