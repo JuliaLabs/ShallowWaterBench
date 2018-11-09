625a5850a89d03b691c0faf852fd94724445dd61
Valentin Churavy <v.churavy@gmail.com>
2018-11-09 10:52:18 -0500

    add Test

---
+++ src/TotallyNotApproxFun/test/runtests.jl
@@ -0,0 +1,22 @@
+using TotallyNotApproxFun
+using Canary
+using Test
+
+@testset "mathtest" begin
+    N = 5
+    dim = 2
+    ξ, w = lglpoints(Float64,N-1)
+    Ψ = ProductBasis(repeat([LagrangeBasis(LobattoPoints(N-1))], dim)...)
+    F = ComboFun(Ψ, [TotallyNotApproxFun.SVector{dim}(randn(dim)) for i in 1:N, j in 1:N])
+    D = TotallyNotApproxFun.spectralderivative(ξ)
+    F1 = getindex.(F.coeffs,1)
+    F2 = getindex.(F.coeffs,2)
+    math∫∇ΨF = [w[j]*F1[:,j]'*(D[:,i].*w)+w[i]*F2[i,:]'*(D[:,j].*w) for i in 1:N, j in 1:N]
+    println("Test ∫∇Ψ")
+    @test math∫∇ΨF ≈ ∫∇Ψ(F).coeffs
+
+    f = ComboFun(Ψ, randn(N,N))
+    math∇f = [TotallyNotApproxFun.SVector(f.coeffs[:,j]'*D[i,:], f.coeffs[i,:]'*D[j,:]) for i in 1:N, j in 1:N]
+    println("Test ∇")
+    @test math∇f == ∇(f).coeffs
+end
